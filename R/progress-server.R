863a4a91c5bb739c5a924890f84c150080df785d
Gábor Csárdi <csardi.gabor@gmail.com>
2026-05-27 19:54:47 +0200

    Fix URL redirect

--- R/progress-server.R
+++ R/progress-server.R
@@ -41,7 +41,7 @@
 #'
 #' ### `rstudio`
 #'
-#' Use [RStudio's job panel](https://posit.co/blog/rstudio-1-2-jobs/)
+#' Use [RStudio's job panel](https://posit.co/blog/rstudio-1-2-jobs)
 #' to show the progress bars. This handler is available at the RStudio console,
 #' in recent versions of RStudio.
 #'
