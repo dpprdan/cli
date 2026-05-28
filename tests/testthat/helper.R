53a03a1d63a045e397ffd3571b60635fec03fc82
Gábor Csárdi <csardi.gabor@gmail.com>
2026-05-28 10:19:08 +0200

    Improve tests
    Better regexp to standardize variable output.

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -80,8 +80,8 @@ test_style <- function() {
 }
 
 fix_times <- function(out) {
-  out <- sub("[(][ ]*[.0-9]+ [Mk]B/s[)]", "(8.5 MB/s)", out)
-  out <- sub("[(][.0-9]+/s[)]", "(100/s)", out)
+  out <- sub("[(][ ]*[.0-9eE+-]+ [Mk]B/s[)]", "(8.5 MB/s)", out)
+  out <- sub("[(][.0-9eE+-]+/s[)]", "(100/s)", out)
   out <- sub(" [.0-9]+(ms|s|m)", " 3ms", out)
   out <- sub("ETA:[ ]*[.0-9]+m?s", "ETA:  1s", out)
   out <- gsub("\\[[.0-9]+m?s\\]", "[1s]", out)
